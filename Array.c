ccc0b7bae96e8a76f5d889e7aa46835eaf2be174
SoorajMS <125868904+sooraj-23MS@users.noreply.github.com>
2024-06-02 11:26:39 +0530

    Array.c

--- Array.c
+++ Array.c
@@ -58,4 +58,43 @@ int main()
  {
    printf("  %d  ",Arr[i]);
  }
+    // 2D Array Creation
+
+ int mat[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+ printf("Print Matrix :\n");
+ for(int i=0;i<3;i++)
+ {
+     for(int j=0;j<3;j++)
+     {
+         printf("%d ",mat[i][j]);
+     }
+     printf("\n");
+ }
+
+ // user input
+ printf("\nUser input Matrix");
+ int row ;
+ int col ;
+ printf("\nEnter row :\n");
+ scanf("%d",&row);
+ printf("Enter column :\n");
+ scanf("%d",&col);
+ int mat1[row][col];
+ printf("\nEnter values :\n");
+ for(int i=0;i<row;i++)
+ {
+     for(int j=0;j<col;j++)
+     {
+         scanf("%d",&mat1[i][j]);
+     }
+ }
+ printf("\nMatrix is :\n");
+ for(int i=0;i<row;i++)
+ {
+     for(int j=0;j<col;j++)
+     {
+         printf("%d ",mat1[i][j]);
+     }
+     printf("\n");
+ }
 }
